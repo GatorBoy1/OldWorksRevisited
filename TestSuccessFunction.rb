a15a50a2de640b06436fc96dcd08b2e9dd1c98a1
Don Kingery <kingery1@suddenlink.net>
2016-10-06 06:27:49 -0400

    PrintTest2equals2

--- TestSuccessFunction.rb
+++ TestSuccessFunction.rb
@@ -5,10 +5,16 @@ class TestSuccessFunction <Minitest::Test
 	def test_1_equals_1
 		assert_equal(1,1)
 	end
+
 def test_1_returns_1
 	#this value in () is the value in variable called in function file
 	assert_equals(1, success(1))
 end
+
+def test_2_returns_2
+	assert_equal(2,success(2))
+end
+
 
 
 
